8c93c4352954a75dfa48a9b7278292af043f98e7
DoJiSeon <djs6620@gmail.com>
2020-12-22 22:56:06 +0900

    프로그래밍 주석

---
+++ ProgrammingHint2/src/Q2/Main.java
@@ -0,0 +1,10 @@
+package Q2;
+
+public class Main {
+	public static void main(String[] args) {
+		 new FirstFrame(); // 蛤ィお
+         new FirstFrame(300, 300);
+         new FirstFrame("六六六六六");
+		
+	}
+}
